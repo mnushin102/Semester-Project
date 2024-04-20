e67cc3b37336863657a07a5381b77a2fe432dd32
Brandon J. Cazares <130398253+BraJCaz@users.noreply.github.com>
2024-04-19 22:19:33 -0700

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,1 +1,16 @@
--- Brandon and Meraj wrote this code 
+"Brandon and Meraj wrote this code" 
+import sys
+import time 
+import getpass
+import socket 
+import threading 
+sys.path.insert(0, '..')
+sys.path.insert(0, '../../python-p2p-network')
+
+from p2psecure.securenode import SecureNode 
+
+host     =
+port     =
+key_file = "secure_node.dat"
+
+if len(sys.argv 
